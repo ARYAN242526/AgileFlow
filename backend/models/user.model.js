64fe725e5a9d714b5ffa644785aab623c6a62bd9
ARYAN242526 <roy2503aryan@gmail.com>
2026-02-15 15:37:00 +0530

    feat : user schema with pre-save hook(hash password) and compare password functionality added

---
+++ backend/models/user.model.js
@@ -0,0 +1,47 @@
+import mongoose, { model } from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true , "Name is required"],
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: [true , "Email is required"],
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: [true , "Password is required"],
+        minLength: 6,
+        select: false, // do not retuern password by default 
+    },
+    role: {
+        type: String,
+        enum : ["Admin", "ProjectManager" , "Developer"],
+        default : "Developer",
+    },
+},
+    {timestamps : true}
+);
+
+// pre-save hook -> hash password
+userSchema.pre("save" , async function (next) {
+    // only hash if password is modified
+    if(!this.isModified("password")) return next();
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password , salt);
+    next();
+})
+
+// compare password method
+userSchema.methods.comparePassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword , this.password);
+};
+
+export const User = mongoose.model("User" , userSchema);
